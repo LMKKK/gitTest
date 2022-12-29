bfeba2d3b58b99e9dc2fde8cf0a8ec5fa905cd73
kkk <1793645613@qq.com>
2022-12-29 15:18:42 +0800

    This is first version!

---
+++ src/main/java/com/GitTest.java
@@ -0,0 +1,14 @@
+package com;
+
+/**
+ * @ClassName : GitTest  //类名
+ * @Description :   //描述
+ * @Author : 刘明凯的专属computer //作者
+ * @Date: 2022/12/29 0029  15:15
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
