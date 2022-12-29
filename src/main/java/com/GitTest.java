a7a1f8ed76adc6c7bad7572497bd62a26f17ad9b
kkk <1793645613@qq.com>
2022-12-29 15:22:34 +0800

    This is second version!

--- src/main/java/com/GitTest.java
+++ src/main/java/com/GitTest.java
@@ -10,5 +10,6 @@ package com;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git!222222222");
     }
 }
